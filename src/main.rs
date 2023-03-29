3113de987d9a852dd0e9b251aa4f0c3a65280bb2
Emil Skogsund <skogix@gmail.com>
2023-03-29 20:47:25 +0200

    basic map and movement

--- src/main.rs
+++ src/main.rs
@@ -1,141 +1,183 @@
 // use/using
-// rltk är roguelike-engine
-use rltk::{Rltk, GameState, RGB, VirtualKeyCode};
+// rltk är roguelite-engine
+use rltk::{GameState, Rltk, RGB, VirtualKeyCode};
 // specs är ECS
-use specs::{prelude::*, world, rayon::iter::Positions};
-// derive är macros för att bara skriva #[derive(Component)]
-use specs_derive::Component;
-// standard library compare
+use specs::prelude::*;
+use specs_derive::*;
+// cmp är compare
 use std::cmp::{max, min};
-// all state / data, lite ledsen att det inte hanteras som immutable
-struct State {
-    ecs: World
-}
-
-// komponenter / data som representerar all data för entities
+// #[derive(x)] är macros
+// gör så man slipper definiera components
 #[derive(Component)]
 struct Position {
     x: i32,
     y: i32,
 }
 #[derive(Component)]
-struct Renderable{
+struct Renderable {
     glyph: rltk::FontCharType,
     fg: RGB,
     bg: RGB,
 }
-#[derive(Component)]
-struct LeftMover {}
-
 #[derive(Component, Debug)]
 struct Player {}
+#[derive(PartialEq, Copy, Clone)]
+enum TileType {
+    Wall, Floor
+}
+// world-state, inte immutable men får väl stå ut
+struct State {
+    ecs: World
+}
+// skapar en unsigned-lista med coords
+// bra för cache-missar
+pub fn xy_idx(x: i32, y: i32) -> usize {
+    (y as usize * 80) + x as usize
+}
+// skapar en ny map som en vec av tiletypes
+fn new_map() -> Vec<TileType> {
+    // 80*50 från rltk:s standard-upplösning
+    let mut map = vec![TileType::Floor; 80*50];
 
-impl<'a> System<'a> for LeftMover {
-    type SystemData = (ReadStorage<'a, LeftMover>, 
-                        WriteStorage<'a, Position>);
+    // skapa väggar
+    for x in 0..80 {
+        map[xy_idx(x, 0)] = TileType::Wall;
+        map[xy_idx(x, 49)] = TileType::Wall;
+    }
+    for y in 0..50 {
+        map[xy_idx(0, y)] = TileType::Wall;
+        map[xy_idx(79, y)] = TileType::Wall;
+    }
 
-    fn run(&mut self, (lefty, mut pos) : Self::SystemData) {
-        for (_lefty,pos) in (&lefty, &mut pos).join() {
-            pos.x -= 1;
-            if pos.x < 0 { pos.x = 79; }
+    // hämtar en rng
+    // tydligen så går det emot rusts regler och följer d&d-regler och är inklusiv
+    let mut rng = rltk::RandomNumberGenerator::new();
+    // sätter randomväggar slumpat över mappen
+    for _i in 0..400 {
+        let x = rng.roll_dice(1, 79);
+        let y = rng.roll_dice(1, 49);
+        let idx = xy_idx(x, y);
+        if idx != xy_idx(40, 25) {
+            map[idx] = TileType::Wall;
         }
     }
-}
 
-impl State {
-    fn run_systems(&mut self){
-        let mut lm = LeftMover{};
-        lm.run_now(&self.ecs);
-        self.ecs.maintain();
-    }
+    map
 }
 
 fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
+    // hämtar mutable kombo av pos och players
+    // är bara player som hämtas och dess pos
     let mut positions = ecs.write_storage::<Position>();
     let mut players = ecs.write_storage::<Player>();
-    
+    // fetchar immutable map
+    let map = ecs.fetch::<Vec<TileType>>();
+    // loopar genom resultat men är player och dess pos
     for (_player, pos) in (&mut players, &mut positions).join() {
-        pos.x = min(79, max(0, pos.x + delta_x));
-        pos.y = min(49, max(0, pos.y + delta_y));
+        // target pos
+        let destination_idx = xy_idx(pos.x + delta_x, pos.y + delta_y);
+        // om target inte är immovable/wall så move
+        if map[destination_idx] != TileType::Wall {
+            pos.x = min(79 , max(0, pos.x + delta_x));
+            pos.y = min(49, max(0, pos.y + delta_y));
+        }
     }
 }
 
 fn player_input(gs: &mut State, ctx: &mut Rltk) {
-    // player movement
+    // movement
     match ctx.key {
         None => {} // inget händer
-        Some(key) => match key {
+        Some(key) => match key { // flytta beroende på input
             VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
             VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
             VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
             VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
-            _ => {}
+            _ => {} // basecase
+        },
+    }
+}
+// render / draw-system för map
+// TODO: varför hämtar vi mutable rltk?
+fn draw_map(map: &[TileType], ctx : &mut Rltk) {
+    let mut y = 0;
+    let mut x = 0;
+    for tile in map.iter() {
+        // rendera tiles beronde på typ
+        match tile {
+            TileType::Floor => {
+                ctx.set(x, y, RGB::from_f32(0.5, 0.5, 0.5), RGB::from_f32(0., 0., 0.), rltk::to_cp437('.'));
+            }
+            TileType::Wall => {
+                ctx.set(x, y, RGB::from_f32(0.0, 1.0, 0.0), RGB::from_f32(0., 0., 0.), rltk::to_cp437('#'));
+            }
+        }
+
+        // Move the coordinates
+        // vi kör något fulhaxx-array som är cache-friendly, så loopa och öka y när x är maxxat
+        x += 1;
+        if x > 79 {
+            x = 0;
+            y += 1;
         }
     }
 }
 
-// implementation av structen state. 
-// objekt har couplead funktionalitet med data men här är det funktioner
-// som kan agera mot data
+// implementation för state
 impl GameState for State {
-    fn tick(&mut self, ctx: &mut Rltk) {
-        ctx.cls(); // rensa skärm
-        player_input(self, ctx);
-        self.run_systems();
-        // hämta read-only alla entities som har en position-component
-        let positions = self.ecs.read_storage::<Position>(); 
-        // hämta read-only alla entities som har en renderable-component
+    // vanlig gametick/update
+    fn tick(&mut self, ctx : &mut Rltk) {
+        ctx.cls(); // clear screen
+        player_input(self, ctx); // hämta input som start
+        self.run_systems(); // kör system
+        let map = self.ecs.fetch::<Vec<TileType>>(); 
+        draw_map(&map, ctx); // efter system är körda så draw map 
+
+        // efter map så dra entities med pos/renderable ovanpå
+        let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
-        // hämta alla entities som har både en position och renderable-component
-        // (blir senare "renderering-system")
+
         for (pos, render) in (&positions, &renderables).join() {
-            // printa dem till skärmen
             ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
         }
-    } 
+    }
+}
+// state påverkas och kör systems
+impl State {
+    fn run_systems(&mut self) {
+        self.ecs.maintain();
+    }
 }
 
-// main med returntype BError
+// main returnar BError men annars vanlig main
 fn main() -> rltk::BError {
-    // tror den hjälper till med builder-pattern, aka .with().with().build()
+    // ger oss builder-pattern
     use rltk::RltkBuilder;
-    // skapa en mutable world state, gs = gamestate
+    // context / ctx är rendering och os-specifikt
+    let context = RltkBuilder::simple80x50()
+        .with_title("Roguelike Tutorial")
+        .build()?;
+    // inita gamestate
     let mut gs = State {
         ecs: World::new()
     };
-    // lägg till så systemet vet vad en position / renderable är
+    // regga alla components
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
-    gs.ecs.register::<LeftMover>();
     gs.ecs.register::<Player>();
-    // skapa en entity med position och renderable
-    // senare läggs nog en "tag" till som säger "player"
-    gs.ecs.create_entity()
-        .with(Position{x:40, y:40})
-        .with(Renderable{
+    // regga resources
+    gs.ecs.insert(new_map());
+    // skapa entities
+    gs.ecs
+        .create_entity()
+        .with(Position { x: 40, y: 25 })
+        .with(Renderable {
             glyph: rltk::to_cp437('@'),
             fg: RGB::named(rltk::YELLOW),
             bg: RGB::named(rltk::BLACK),
         })
         .with(Player{})
         .build();
-    // skapar 10 "monster"
-    for i in 0..=5 {
-        gs.ecs
-        .create_entity()
-        .with(Position{x: i * 3 + 30, y: 20})
-        .with(Renderable{
-            glyph: rltk::to_cp437('r'),
-            fg: RGB::named(rltk::RED),
-            bg: RGB::named(rltk::BLACK),
-        })
-        .with(LeftMover{})
-        .build();
-    }
-    // skapar fönster och sköter all magi med rendering
-    let context = RltkBuilder::simple80x50()
-        .with_title("Rustlike")
-        .build()?;
-    // starta game-loopen med rendering-data/context och gamestate / world
+    // kör main-loop med context och gamestate
     rltk::main_loop(context, gs)
 }
