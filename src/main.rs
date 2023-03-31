7651b6a5d9741ce0ffc9dfdd71fae0db5d26fdb9
Emil Skogsund <skogix@gmail.com>
2023-03-31 18:36:04 +0200

    monster ai reagerar när i view

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rltk::{GameState, Rltk, RGB};
+use rltk::{GameState, Rltk, RGB, Point};
 use specs::prelude::*;
 
 // pub används mellan moduler
@@ -103,6 +103,7 @@ fn main() -> rltk::BError {
             .build();
     }
     gs.ecs.insert(map);
+    gs.ecs.insert(Point::new(player_x, player_y));
 
     gs.ecs
         .create_entity()
--- src/player.rs
+++ src/player.rs
@@ -1,4 +1,4 @@
-use rltk::{VirtualKeyCode, Rltk};
+use rltk::{VirtualKeyCode, Rltk, Point};
 use specs::prelude::*;
 use super::{Position, Player, TileType, State, Map, Viewshed, RunState};
 use std::cmp::{min, max};
@@ -16,6 +16,9 @@ pub fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
             pos.x = min(79 , max(0, pos.x + delta_x));
             pos.y = min(49, max(0, pos.y + delta_y));
             viewshed.dirty = true;
+            let mut ppos = ecs.write_resource::<Point>();
+            ppos.x = pos.x;
+            ppos.y = pos.y;
         }
     }
 }
