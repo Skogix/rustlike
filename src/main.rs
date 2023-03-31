671455b4f6696585003ef78c9937b755539dbd4f
Emil Skogsund <skogix@gmail.com>
2023-03-31 18:01:09 +0200

    addat enemies g/o och sätta viewable

--- src/main.rs
+++ src/main.rs
@@ -37,9 +37,13 @@ impl GameState for State {
         draw_map(&self.ecs, ctx);
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
+        let map = self.ecs.fetch::<Map>();
 
         for (pos, render) in (&positions, &renderables).join() {
-            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+            let idx = map.xy_idx(pos.x, pos.y);
+            if map.visible_tiles[idx] {
+                ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+            }
         }
     }
 }
@@ -61,6 +65,29 @@ fn main() -> rltk::BError {
     let map : Map = Map::new_map_rooms_and_corridors();
     // sätt player i mitten av första rummet skapat
     let (player_x, player_y) = map.rooms[0].center();
+    // spawna enemies
+    let mut rng = rltk::RandomNumberGenerator::new();
+    for room in map.rooms.iter().skip(1) {
+        let (x,y) = room.center();
+        let glyph : rltk::FontCharType;
+        let roll = rng.roll_dice(1,2);
+        match roll {
+            1 => { glyph = rltk::to_cp437('g') }
+            _ => { glyph = rltk::to_cp437('o') }
+        }
+        gs.ecs.create_entity()
+            .with(Position{x,y})    
+            .with(Renderable{
+                glyph: glyph,
+                fg: RGB::named(rltk::RED),
+                bg: RGB::named(rltk::BLACK),
+            })
+            .with(Viewshed{ 
+                visible_tiles: Vec::new(), 
+                range: 8, 
+                dirty: true })
+            .build();
+    }
     gs.ecs.insert(map);
 
     gs.ecs
@@ -74,6 +101,7 @@ fn main() -> rltk::BError {
         .with(Player{})
         .with(Viewshed{ visible_tiles: Vec::new(), range: 8, dirty: true })
         .build();
+
 
     rltk::main_loop(context, gs)
 }
